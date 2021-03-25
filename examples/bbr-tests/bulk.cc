19a12fff242f83d4870c80a6f2b521da6f0eec9e
Tiu Wee Han <deantiu56@gmail.com>
2021-03-25 21:21:04 +0800

    Convert to multi-flow experiment

--- examples/bbr-tests/bulk.cc
+++ examples/bbr-tests/bulk.cc
@@ -30,7 +30,7 @@ using namespace ns3;
 #define ENABLE_PCAP      false     // Set to "true" to enable pcap
 #define ENABLE_TRACE     false     // Set to "true" to enable trace
 #define BIG_QUEUE        2000      // Packets
-#define QUEUE_SIZE       1        // Packets
+#define QUEUE_SIZE       120        // Packets
 #define START_TIME       0.0       // Seconds
 #define STOP_TIME        120.0       // Seconds
 #define S_TO_R_BW        "1000Mbps" // Server to router
@@ -40,12 +40,18 @@ using namespace ns3;
 #define PACKET_SIZE      1000      // Bytes.
 
 // Uncomment one of the below.
-#define TCP_PROTOCOL     "ns3::TcpCubic"
-//#define TCP_PROTOCOL     "ns3::TcpNewReno"
+
+enum TCP_PROTOCOL { BBR, CUBIC };
+
+typedef struct {
+  TCP_PROTOCOL prot;
+  int rtt;  
+} flow_info;
 
 // For logging. 
 
 NS_LOG_COMPONENT_DEFINE ("main");
+
 /////////////////////////////////////////////////
 int main (int argc, char *argv[]) {
 
@@ -56,23 +62,43 @@ int main (int argc, char *argv[]) {
   LogComponentEnable("main", LOG_LEVEL_INFO);
 
   /////////////////////////////////////////
-  // Setup environment
-  Config::SetDefault("ns3::TcpL4Protocol::SocketType",
-                     StringValue(TCP_PROTOCOL));
+  // Read input
+  std::string flows = "BCBCBC";
+  std::vector<int> rtts { 20, 50, 80 };
 
-  Config::SetDefault ("ns3::PfifoFastQueueDisc::Limit", UintegerValue (125));
+  int packetSize = 1000;
+  double bdp = 1;
+  int bandwidth = 20;
+  int maxRtt = *std::max_element(rtts.begin(), rtts.end());
+  int queueSizeBytes = bdp * bandwidth * maxRtt * 1000 / 8;
+  int queueSizePackets = queueSizeBytes / packetSize;
+  
+  int nSender = flows.size();
+  std::vector<flow_info> flow_infos(nSender);
+  for (int i = 0; i < nSender; i++) {
+    flow_infos[i] = flow_info { flows[i] == 'B' ? BBR : CUBIC, rtts[i / (nSender / rtts.size())] };
+  }
+
+  CommandLine cmd;
+  cmd.AddValue("flows", "Flow combinations of BBR and Cubic", flows);
+  cmd.AddValue("bdp", "BDP", bdp);
+  cmd.AddValue("bandwidth", "Bandwidth", bandwidth);
+  cmd.Parse(argc, argv);
+
+  /////////////////////////////////////////
+  // Setup environment
+  Config::SetDefault ("ns3::PfifoFastQueueDisc::Limit", UintegerValue (queueSizePackets / 2));
 
   // Report parameters.
-  NS_LOG_INFO("TCP protocol: " << TCP_PROTOCOL);
+  NS_LOG_INFO("Flow: " << flows);
   NS_LOG_INFO("Server to Router Bwdth: " << S_TO_R_BW);
   NS_LOG_INFO("Server to Router Delay: " << S_TO_R_DELAY);
-  NS_LOG_INFO("Router to Router Bwdth: " << R_TO_R_BW);
-  NS_LOG_INFO("Router to Router Delay: " << R_TO_R_DELAY);
-  NS_LOG_INFO("Packet size (bytes): " << PACKET_SIZE);
+  NS_LOG_INFO("Router to Router Bwdth: " << bandwidth << "Mbps");
+  NS_LOG_INFO("Packet size (bytes): " << packetSize);
+  NS_LOG_INFO("Queue Size (packets): " << queueSizePackets);
   
   // Set segment size (otherwise, ns-3 default is 536).
-  Config::SetDefault("ns3::TcpSocket::SegmentSize",
-                     UintegerValue(PACKET_SIZE)); 
+  Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(packetSize)); 
 
   // Turn off delayed ack (so, acks every packet).
   // Note, BBR' still works without this.
@@ -80,135 +106,133 @@ int main (int argc, char *argv[]) {
    
   /////////////////////////////////////////
   // Create nodes.
-  NS_LOG_INFO("Creating nodes.");
-  NodeContainer nodes1;
-  nodes1.Create(2);
-  NodeContainer nodes2;
-  nodes2.Create(2);
-  NodeContainer nodes3;
-  nodes3.Create(2);
+  // NS_LOG_INFO("Creating nodes.");
 
-  NodeContainer nodes;  // 0=router_left 1=router_right 2=bbr_source 3=bbr_sink 4=cubic_source 5=cubic_sink
-  nodes.Add(nodes1);
-  nodes.Add(nodes2);
-  nodes.Add(nodes3);
+  NodeContainer routerNodes; 
+  routerNodes.Create(2);
+  NodeContainer senderNodes;
+  senderNodes.Create(nSender);
+  NodeContainer receiverNodes;
+  receiverNodes.Create(nSender);
 
   /////////////////////////////////////////
   // Create channels.
-  NS_LOG_INFO("Creating channels.");
-  NodeContainer r0_to_r1  = NodeContainer(nodes.Get(0), nodes.Get(1));
-  NodeContainer n2_to_r0 = NodeContainer(nodes.Get(2), nodes.Get(0));
-  NodeContainer r1_to_n3 = NodeContainer(nodes.Get(1), nodes.Get(3));
-  NodeContainer n4_to_r0 = NodeContainer(nodes.Get(4), nodes.Get(0));
-  NodeContainer r1_to_n5 = NodeContainer(nodes.Get(1), nodes.Get(5));
+  // NS_LOG_INFO("Creating channels.");
 
   /////////////////////////////////////////
   // Create links.
-  NS_LOG_INFO("Creating links.");
+  // NS_LOG_INFO("Creating links.");
 
   PointToPointHelper p2p;
-  int mtu = 1500;
+  int mtu = 2000;
 
   // Router to Router.
-  p2p.SetDeviceAttribute("DataRate", StringValue (R_TO_R_BW));
+  NodeContainer r0_to_r1  = NodeContainer(routerNodes.Get(0), routerNodes.Get(1));
+  std::string dataRate = std::to_string(bandwidth) + "Mbps";
+  p2p.SetDeviceAttribute("DataRate", StringValue (dataRate));
   p2p.SetChannelAttribute("Delay", StringValue (R_TO_R_DELAY));
   p2p.SetDeviceAttribute ("Mtu", UintegerValue(mtu));
-  NS_LOG_INFO("Router queue size: "<< QUEUE_SIZE);
+  // NS_LOG_INFO("Router queue size: "<< QUEUE_SIZE);
   p2p.SetQueue("ns3::DropTailQueue",
                "Mode", StringValue ("QUEUE_MODE_PACKETS"),
-               "MaxPackets", UintegerValue(QUEUE_SIZE));
-  NetDeviceContainer devices_r0_to_r1 = p2p.Install(r0_to_r1);
+               "MaxPackets", UintegerValue(queueSizePackets / 2));
+  NetDeviceContainer routerDevices = p2p.Install(r0_to_r1);
+
+  NetDeviceContainer senderDevices, router0Devices, router1Devices, receiverDevices;
 
   // BBR Server to Router.
-  p2p.SetDeviceAttribute("DataRate", StringValue (S_TO_R_BW));
-  p2p.SetChannelAttribute("Delay", StringValue (S_TO_R_DELAY));
-  p2p.SetDeviceAttribute ("Mtu", UintegerValue(mtu));
-  NetDeviceContainer devices_n2_to_r0 = p2p.Install(n2_to_r0);
-  NetDeviceContainer devices_r1_to_n3 = p2p.Install(r1_to_n3);
-  NetDeviceContainer devices_n4_to_r0 = p2p.Install(n4_to_r0);
-  NetDeviceContainer devices_r1_to_n5 = p2p.Install(r1_to_n5);
+  for (int i = 0; i < nSender; i++) {
+    std::string delay = std::to_string(flow_infos[i].rtt / 2) + "ms";
+    p2p.SetDeviceAttribute("DataRate", StringValue (S_TO_R_BW));
+    p2p.SetChannelAttribute("Delay", StringValue (delay));
+    p2p.SetDeviceAttribute ("Mtu", UintegerValue(mtu));
+    NodeContainer s_to_r0 = NodeContainer(senderNodes.Get(i), routerNodes.Get(0));
+    NodeContainer r1_to_c = NodeContainer(routerNodes.Get(1), receiverNodes.Get(i));
+    NetDeviceContainer leftDevices = p2p.Install(s_to_r0);
+    senderDevices.Add(leftDevices.Get(0));
+    router0Devices.Add(leftDevices.Get(1));
+    NetDeviceContainer rightDevices = p2p.Install(r1_to_c);
+    router1Devices.Add(rightDevices.Get(0));
+    receiverDevices.Add(rightDevices.Get(1));
+  }
 
   /////////////////////////////////////////
   // Install Internet stack.
-  NS_LOG_INFO("Installing Internet stack.");
+  // NS_LOG_INFO("Installing Internet stack.");
   InternetStackHelper internet;
-  internet.Install(nodes1);
+  internet.Install(routerNodes);
 
-  Config::SetDefault("ns3::TcpL4Protocol::SocketType",
-                    StringValue("ns3::TcpBbr"));
-  internet.Install(nodes2);
-
-  Config::SetDefault("ns3::TcpL4Protocol::SocketType",
-                    StringValue("ns3::TcpCubic"));
-  internet.Install(nodes3);
+  for (int i = 0; i < nSender; i++) {
+    switch (flow_infos[i].prot)
+    {
+    case BBR:
+      Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpBbr"));
+      break;
+    case CUBIC:
+      Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpCubic"));
+      break;
+    default:
+      break;
+    }
+    internet.Install(senderNodes.Get(i));
+    internet.Install(receiverNodes.Get(i));
+  }
 
   /////////////////////////////////////////
   // Add IP addresses.
-  NS_LOG_INFO("Assigning IP Addresses.");
+  // NS_LOG_INFO("Assigning IP Addresses.");
 
   Ipv4AddressHelper ipv4;
   ipv4.SetBase("172.16.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer i0i1 = ipv4.Assign(devices_r0_to_r1);
-  
-  ipv4.SetBase("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer i2i0 = ipv4.Assign(devices_n2_to_r0);
+  Ipv4InterfaceContainer i0i1 = ipv4.Assign(routerDevices);
+  Ipv4InterfaceContainer iSenders, iRouter0, iRouter1, iReceivers;
 
-  ipv4.SetBase("191.168.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer i1i3 = ipv4.Assign(devices_r1_to_n3);
+  for (int i = 0; i < nSender; i++) {
+    std::string addr = "10.1." + std::to_string(i) + ".0";
+    ipv4.SetBase(addr.c_str(), "255.255.255.0");
+    NetDeviceContainer leftDevices(senderDevices.Get(i), router0Devices.Get(i));
+    Ipv4InterfaceContainer iLeft = ipv4.Assign(leftDevices);
+    iSenders.Add(iLeft.Get(0));
+    iRouter0.Add(iLeft.Get(1));
 
-  ipv4.SetBase("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer i4i0 = ipv4.Assign(devices_n4_to_r0);
-
-  ipv4.SetBase("191.168.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer i1i5 = ipv4.Assign(devices_r1_to_n5);
+    addr = "192.168." + std::to_string(i) + ".0";
+    ipv4.SetBase(addr.c_str(), "255.255.255.0"); 
+    NetDeviceContainer rightDevices(router1Devices.Get(i), receiverDevices.Get(i));
+    Ipv4InterfaceContainer iRight = ipv4.Assign(rightDevices);
+    iRouter1.Add(iRight.Get(0));
+    iReceivers.Add(iRight.Get(1));
+  }
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
   /////////////////////////////////////////
   // Create apps.
-  NS_LOG_INFO("Creating applications.");
-  NS_LOG_INFO("  Bulk send.");
+  // NS_LOG_INFO("Creating applications.");
+  // NS_LOG_INFO("  Bulk send.");
 
   // Well-known port for server.
   uint16_t port = 911;  
 
-  ApplicationContainer apps;
+  std::vector<Ptr<PacketSink>> sinks(nSender);
 
-  // BBR Source (at node 2).
-  BulkSendHelper bbr_source("ns3::TcpSocketFactory",
-                        InetSocketAddress(i1i3.GetAddress(1), port));
-  // Set the amount of data to send in bytes (0 for unlimited).
-  bbr_source.SetAttribute("MaxBytes", UintegerValue(0));
-  bbr_source.SetAttribute("SendSize", UintegerValue(PACKET_SIZE));
-  apps = bbr_source.Install(nodes.Get(2));
-  apps.Start(Seconds(START_TIME));
-  apps.Stop(Seconds(STOP_TIME));
+  for (int i = 0; i < nSender; i++) {
+    ApplicationContainer apps;
+  
+    BulkSendHelper source("ns3::TcpSocketFactory", InetSocketAddress(iReceivers.GetAddress(i), port));
+    // Set the amount of data to send in bytes (0 for unlimited).
+    source.SetAttribute("MaxBytes", UintegerValue(0));
+    source.SetAttribute("SendSize", UintegerValue(PACKET_SIZE));
+    apps = source.Install(senderNodes.Get(i));
+    apps.Start(Seconds(START_TIME));
+    apps.Stop(Seconds(STOP_TIME));
 
-  // BBR Sink (at node 3).
-  PacketSinkHelper bbr_sink("ns3::TcpSocketFactory",
-                        InetSocketAddress(Ipv4Address::GetAny(), port));
-  apps = bbr_sink.Install(nodes.Get(3));
-  apps.Start(Seconds(START_TIME));
-  apps.Stop(Seconds(STOP_TIME));
-  Ptr<PacketSink> p_sink_bbr = DynamicCast<PacketSink> (apps.Get(0)); // 4 stats
-
-  // CUBIC Source (at node 4).
-  BulkSendHelper cubic_source("ns3::TcpSocketFactory",
-                        InetSocketAddress(i1i5.GetAddress(1), port));
-  // Set the amount of data to send in bytes (0 for unlimited).
-  cubic_source.SetAttribute("MaxBytes", UintegerValue(0));
-  cubic_source.SetAttribute("SendSize", UintegerValue(PACKET_SIZE));
-  apps = cubic_source.Install(nodes.Get(4));
-  apps.Start(Seconds(START_TIME));
-  apps.Stop(Seconds(STOP_TIME));
-
-  // CUBIC Sink (at node 5).
-  PacketSinkHelper cubic_sink("ns3::TcpSocketFactory",
-                        InetSocketAddress(Ipv4Address::GetAny(), port));
-  apps = cubic_sink.Install(nodes.Get(5));
-  apps.Start(Seconds(START_TIME));
-  apps.Stop(Seconds(STOP_TIME));
-  Ptr<PacketSink> p_sink_cubic = DynamicCast<PacketSink> (apps.Get(0)); // 4 stats
+    // Packet Sink at Receiver
+    PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    apps = sink.Install(receiverNodes.Get(i));
+    apps.Start(Seconds(START_TIME));
+    apps.Stop(Seconds(STOP_TIME));
+    sinks[i] = DynamicCast<PacketSink> (apps.Get(0)); // 4 stats
+  }
 
   /////////////////////////////////////////
   // Setup tracing (as appropriate).
@@ -224,33 +248,27 @@ int main (int argc, char *argv[]) {
 
   /////////////////////////////////////////
   // Run simulation.
-  NS_LOG_INFO("Running simulation.");
+  // NS_LOG_INFO("Running simulation.");
   Simulator::Stop(Seconds(STOP_TIME));
   NS_LOG_INFO("Simulation time: [" << 
               START_TIME << "," <<
               STOP_TIME << "]");
-  NS_LOG_INFO("---------------- Start -----------------------");
+  // NS_LOG_INFO("---------------- Start -----------------------");
   Simulator::Run();
-  NS_LOG_INFO("---------------- Stop ------------------------");
+  // NS_LOG_INFO("---------------- Stop ------------------------");
 
   /////////////////////////////////////////
   // Ouput stats.
   double tput;
 
-  NS_LOG_INFO("----------------- BBR ------------------------");
-  NS_LOG_INFO("Total bytes received: " << p_sink_bbr->GetTotalRx());
-  tput = p_sink_bbr->GetTotalRx() / (STOP_TIME - START_TIME);
-  tput *= 8;          // Convert to bits.
-  tput /= 1000000.0;  // Convert to Mb/s
-  NS_LOG_INFO("Throughput: " << tput << " Mb/s");
-  
-  NS_LOG_INFO("----------------- CUBIC ----------------------");
-  NS_LOG_INFO("Total bytes received: " << p_sink_cubic->GetTotalRx());
-  tput = p_sink_cubic->GetTotalRx() / (STOP_TIME - START_TIME);
-  tput *= 8;          // Convert to bits.
-  tput /= 1000000.0;  // Convert to Mb/s
-  NS_LOG_INFO("Throughput: " << tput << " Mb/s");
-  NS_LOG_INFO("Done.");
+  for (int i = 0; i < nSender; i++) {
+    NS_LOG_INFO("----------------- Flow " << i << ": " << flow_infos[i].prot << ", RTT " << flow_infos[i].rtt << "ms ------------------------");
+    NS_LOG_INFO("Total bytes received: " << sinks[i]->GetTotalRx());
+    tput = sinks[i]->GetTotalRx() / (STOP_TIME - START_TIME);
+    tput *= 8;          // Convert to bits.
+    tput /= 1000000.0;  // Convert to Mb/s
+    NS_LOG_INFO("Throughput: " << tput << " Mb/s");
+  }
 
   // Done.
   Simulator::Destroy();
